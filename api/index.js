a70ff600e587048356b1cd96257c76224ed3754f
arunhari0201-dev <arunhari0201@gmail.com>
2025-12-20 18:03:10 +0530

    Fix CORS: Allow all headers to prevent preflight errors

--- api/index.js
+++ api/index.js
@@ -114,7 +114,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie', 'Cache-Control', 'Pragma', 'X-Requested-With'],
+  allowedHeaders: '*',
 }));
 
 // Handle preflight requests explicitly
